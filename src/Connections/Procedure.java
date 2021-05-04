b7a2877892e65e41732d8233c8b60635ce33a59f
BrandonGC10 <bran.101015@gmail.com>
2021-05-03 20:00:23 -0500

    Agregue mis procedimientos

--- src/Connections/Procedure.java
+++ src/Connections/Procedure.java
@@ -30,4 +30,28 @@ public class Procedure {
         input.setInt(1, a);
         input.execute();
     }
+    
+    public static void AgregarProveedor(int id_Provee, String nombre_Provee, String empresa_Provee
+            , String telefono_Provee, String fecha_Provee, String direccion_Provee) throws SQLException {
+        CallableStatement input = Connectionn.getConnection().prepareCall("{call EntradaUsuario(?,?,?,?,?,?,?,?,?)}");
+        input.setInt(1, id_Provee);
+        input.setString(2, nombre_Provee);
+        input.setString(3, empresa_Provee);
+        input.setString(4, telefono_Provee);
+        input.setString(5, fecha_Provee);
+        input.setString(6, direccion_Provee);
+        input.execute();
+    }
+    
+     public static void EliminarProveedor(int id_Provee) throws SQLException {
+        CallableStatement input = Connectionn.getConnection().prepareCall("{call EliminarUsuario(?)}");
+        input.setInt(1, id_Provee);
+        input.execute();
+    }
+
+    public static void BuscarProveedor(int id_Provee) throws SQLException {
+        CallableStatement input = Connectionn.getConnection().prepareCall("{call BuscarUsuario(?)}");
+        input.setInt(1, id_Provee);
+        input.execute();
+    }
 }
